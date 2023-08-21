4e766580e640e75eec07974f5a9dcf72531851cd
Daniel Boyé <boyelanrewaju@gmail.com>
2023-08-21 07:07:23 +0000

    change permission

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Defines a function that determines if a box containing a list
    of lists can be opened using keys stored in the lists
-"""
+   """
 
 
 def canUnlockAll(boxes):
